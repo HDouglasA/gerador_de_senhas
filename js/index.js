4effef94dc5e11e45ca9a0a0d5d6a10e79c52221
HDouglasA <araujohdouglas@gmail.com>
2022-10-02 17:57:55 -0300

    Projeto Finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-console.log("teste");
+const obterLetraMinuscula = () => {
+  return String.fromCharCode(Math.floor(Math.random()*26) +97)
+}
+
+const obterLetraMaiuscula = () => {
+  return String.fromCharCode(Math.floor(Math.random()*26) +65)
+}
+
+const obterNumero = () => {
+  return Math.floor(Math.random()*10).toString()
+}
+
+const obtersimbolo = () => {
+  let simbolos = "[]{}()!@#$%&*/?|+,.<>="
+  return simbolos[Math.floor(Math.random()*simbolos.length)] 
+}
+
+const gerarSenha = (funcao01, funcao02, funcao03, funcao04) => {
+
+  let senha = ""
+  const tamanhoSenha = 12
+
+  const funcoes = [funcao01, funcao02, funcao03, funcao04]
+
+  for (let i= 0; i < tamanhoSenha; i += gerarSenha.length ) {
+    funcoes.forEach(()=> {
+      const valorSenha =funcoes[Math.floor(Math.random() *funcoes.length)]()
+      senha += valorSenha
+    });
+  }
+  let ElementoSenhaGerada= document.getElementById("senha-gerada")
+  ElementoSenhaGerada.style.display = "block"
+  ElementoSenhaGerada.querySelector("h4").innerText = senha
+}
+
+const obterSenha = () => {
+  gerarSenha(obterLetraMinuscula, obterLetraMaiuscula, obterNumero, obtersimbolo)
+}
